13849f9de0478df7e76a33d861c8022c30113562
Ross DuPaw <rdupaw@drivindealer.com>
2017-10-20 16:07:40 -0500

    Create form component to pull user data from GH API and add to state

---
+++ src/Form.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export default class Form extends React.Component {
+  state = { userName: '' }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    axios.get(`https://api.github.com/users/${ this.state.userName }`).then(resp => {
+        this.props.onSubmit(resp.data)
+        this.setState({ userName: '' });
+    });
+  };
+
+  render () {
+    return (
+      <form onSubmit={ this.handleSubmit }>
+        <input type='text' 
+          value={ this.state.userName }
+          onChange={(event) => this.setState({ userName: event.target.value })}
+          placeholder='Github username' required />
+        <button type='submit'>Add Card</button>
+      </form>
+    );
+  }
+}
