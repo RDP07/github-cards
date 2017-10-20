a908e9be2d7f277beee516437ee03ba1c9f3fd92
Ross DuPaw <rdupaw@drivindealer.com>
2017-10-20 16:09:49 -0500

    Edit index.js to recieve components, add state, create addNewCard function to update state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Cards from './Cards'
+import CardList from './CardList'
+import Form from './Form'
+
+
+
+class App extends React.Component {
+  state = {
+    cards: []
+  };
+
+  addNewCard = (cardInfo) => {
+    console.log(cardInfo)
+    this.setState(prevState => ({
+      cards: prevState.cards.concat(cardInfo)
+    }))
+    
+  }
+
+  render () {
+    return (
+      <div>
+        <Form onSubmit={ this.addNewCard }/>
+        <CardList cards={ this.state.cards } />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
